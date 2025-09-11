77117dd02372f729b788288102df561897651c73
Rakotoarilala51 <maitreaina51@gmail.com>
2025-09-11 17:28:56 +0300

    feat(subcommand/all.go): creation de sous command all

---
+++ subcommand/all.go
@@ -0,0 +1,14 @@
+package subcommand
+
+import (
+	"github.com/Rakotoarilala51/akata-commit-checker/internal"
+	"github.com/spf13/cobra"
+)
+
+var AllCmd = &cobra.Command{
+	Use: "a",
+	Short: "Evaluation de tous les commits dans tous les branches",
+	Run: func(cmd *cobra.Command, args []string) {
+		internal.GetCommitList()
+	},
+}
