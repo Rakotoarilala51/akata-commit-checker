01f4b38a7ff7c1f0ab6c7fe55ca9af12abda994a
Rakotoarilala51 <maitreaina51@gmail.com>
2025-09-13 15:31:57 +0300

    feat(commitGetter): ajouter le fonction qui va fetcher les commit dans un branches

--- internal/commitGetter.go
+++ internal/commitGetter.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"strings"
 
+	"github.com/go-git/go-git/v6/plumbing"
 	git "github.com/go-git/go-git/v6"
 	"github.com/go-git/go-git/v6/plumbing/object"
 )
@@ -16,7 +17,13 @@ func GetCommitList() {
 		commit.DisplayQualityReport()
 	}
 }
-
+func GetCommitListOfBranch(branchName string) {
+	gitCommitList := getGitCommitListFromBranch(branchName)
+	commitList := convertToCustomCommit(gitCommitList)
+	for _, commit := range commitList{
+		commit.DisplayQualityReport()
+	}
+}
 func getGitCommitList()[]*object.Commit{
 	repo, err := git.PlainOpen(".")
 	if err != nil {
@@ -40,7 +47,31 @@ func getGitCommitList()[]*object.Commit{
 	}
 	return gitCommits
 }
-
+func getGitCommitListFromBranch(branchName string) []*object.Commit {
+	repo, err := git.PlainOpen(".")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	ref, err := repo.Reference(plumbing.ReferenceName("refs/heads/"+branchName), true)
+	if err != nil {
+		log.Fatalf("Branche '%s' introuvable: %v", branchName, err)
+	}
+	iter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	
+	gitCommits := []*object.Commit{}
+	err = iter.ForEach(func(c *object.Commit) error {
+		gitCommits = append(gitCommits, c)
+		return nil
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	
+	return gitCommits
+}
 func convertToCustomCommit(gitCommits []*object.Commit) []Commit {
 	customCommitList := []Commit{}
 	for _, gitCommit := range gitCommits {
